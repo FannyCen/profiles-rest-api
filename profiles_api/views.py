eabdb4966ce9a0b1309447a9cf8f022ce3f53268
Fanny <fanny@subrosmart.com>
2021-11-09 14:56:35 -0600

    Add POST,PUT,PATCH,and DELETE methods to our APIView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,16 +1,74 @@
 from rest_framework.views import APIView
 from rest_framework.views import Response
-
-
+from  rest_framework import status
+from profiles_api import serializers
+# api/hellow-view
 class HelloApiView(APIView):
     """Test API view"""
-    def get(self,request,format=None):
-     """ Returns a list of APIView features"""
-     an_apiview = [
-         'Uses HTTP methods as function(get, post, patch, put, delete)',
-         'Is similar to traditional Django View',
-         'Gives you the most contral over your appointment',
-         'Is mapped manually to URLs',
-     ]
+    serializer_class = serializers.HelloSerializer
 
-     return Response ({'message':'hello','an_apiview': an_apiview})
+
+
+    def get(self, request, format=None):
+        """Returns a list of APIView features"""
+
+        an_apiview = [
+            'Uses HTTP methods as functions (get, post, patch, put, delete)',
+            'Is similar to a traditional Django View',
+            'Gives you the most control over your logic',
+            'Is mapped manually to URLs',
+        ]
+
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}!'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+
+    def put(self, request, pk=None):
+        return Response({'method': 'PUT'})
+
+
+    def patch(self, request, pk=None):
+        return Response({'method': 'PATCH'})
+        """Handle partial update of object"""
+
+    def delete(self, request, pk=None):
+        return Response({'method': 'DELETE'})
+        """Delete an object"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     # validation
+     # # XXX:
+     # db acess
+     # entity(first_name) - db model one to one match profile table, first_name column
+     # save to db profile table
